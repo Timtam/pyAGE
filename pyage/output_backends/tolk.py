411dda89ce31c58b82fdcd9e17ab59fa7501d9e3
Toni Barth <software@satoprogs.de>
2020-10-10 03:52:20 +0200

    removed sys.path modification by tolk output backend since it didn't work either

--- pyage/output_backends/tolk.py
+++ pyage/output_backends/tolk.py
@@ -1,16 +1,9 @@
-import os.path
-import sys
-
 from cytolk import tolk
 
 from pyage.output_backend import OutputBackend
 
 
 class Tolk(OutputBackend):
-    def __init__(self) -> None:
-
-        sys.path.insert(0, os.path.dirname(tolk.__file__))
-
     def Load(self) -> None:
         tolk.load()
 
