ad6687b2db8b5b1fc23cf248b9ba0b4b855c24a7
Toni Barth <software@satoprogs.de>
2020-11-28 22:40:04 +0100

    add documentation for screen stack

--- docs/source/api.rst
+++ docs/source/api.rst
@@ -6,8 +6,24 @@ Developer Interface
 App: Where verything begins
 ---------------------------
 
-**pyAGE** provides a class which runs the event processor and runs the entire game in general. This class is documented as follows:
+**pyAGE** provides a class which runs the event processor and runs the entire 
+game in general. This class is documented as follows:
 
 .. autoclass:: pyage.app.App
+   :inherited-members:
+
+
+ScreenStack: changing the sound and feel of your game
+-----------------------------------------------------
+
+By accessing the :class:`pyage.screen_stack.ScreenStack` instance through 
+:attr:`pyage.app.App.screen_stack`, you can 
+:meth:`pyage.screen_stack.ScreenStack.push` new screens on top of the screen 
+stack or :meth:`pyage.screen_stack.ScreenStack.pop` unwanted screens from the 
+top of the screen stack at any time. The top-most screen will receive all 
+events the game has to offer, including :class:`pyage.events.focus.FocusEvent` 
+and :class:`pyage.events.key.KeyEvent`.
+
+.. autoclass:: pyage.screen_stack.ScreenStack
    :inherited-members:
 
--- pyage/screen_stack.py
+++ pyage/screen_stack.py
@@ -1,21 +1,37 @@
-from typing import TYPE_CHECKING, List
+from typing import List
+
+import pyage.app
 
 from .screen import Screen
-
-if TYPE_CHECKING:
-    from pyage.app import App
 
 
 class ScreenStack:
 
-    _app: "App"
+    _app: "pyage.app.App"
     _screen_stack: List[Screen] = []
 
-    def __init__(self, app: "App") -> None:
+    def __init__(self, app: "pyage.app.App") -> None:
 
         self._app = app
 
     def push(self, screen: Screen) -> None:
+        """
+        This method will push a new screen on top of the screen stack.
+
+        Parameters
+        ----------
+        screen
+
+            a new screen, which will automatically receive the game focus
+
+
+        Event callbacks
+
+        - :meth:`pyage.screen.Screen.shown` will be called in the pushed
+          screen to indicate that the screen was pushed onto the stack
+        - :meth:`pyage.screen.Screen.hidden` will be called in the previously
+          top-most screen (if available) to indicate that it is no longer shown
+        """
 
         if len(self._screen_stack) > 0:
             self._screen_stack[-1].hidden(False)
@@ -25,6 +41,27 @@ class ScreenStack:
         screen.shown(True)
 
     def pop(self) -> bool:
+        """
+        Removes the top-most screen from the stack and moves focus to the next
+        screen on the stack.
+
+        Returns
+        -------
+        bool
+
+            :obj:`True` if a screen was popped, :obj:`False` if there was no
+            screen to be popped.
+
+
+        Event callbacks
+
+        - :meth:`pyage.screen.Screen.shown` will be called in the screen that
+          is now the top-most screen on the stack to indicate that it is now
+          visible again
+        - :meth:`pyage.screen.Screen.hidden` will be called in the popped
+          screen to indicate that its not visible anymore
+
+        """
 
         screen: Screen
 
