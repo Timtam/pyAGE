e8f3398c2ca51c0646055ab37bf0c3fc6731c459
Quin <thequinbox@users.noreply.github.com>
2022-03-09 11:16:01 -0700

    Add accessible_output for speaking to screen readers.

---
+++ pyage/output_backends/ao2.py
@@ -0,0 +1,14 @@
+import accessible_output2.outputs
+
+from pyage.output_backend import OutputBackend
+
+
+class Ao2(OutputBackend):
+    def __init__(self) -> None:
+        self.speaker = accessible_output2.outputs.auto.Auto()
+
+    def __del__(self) -> None:
+        self.speaker = None
+
+    def output(self, text: str, interrupt: bool = True) -> None:
+        speaker.output(text, interrupt)
