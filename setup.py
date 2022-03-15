3db467f4e36a05e1f2373bfd8af97c785d2567b2
Toni Barth <software@satoprogs.de>
2022-03-15 20:34:08 +0100

    change from requirements.txt over to setup.cfg (no need to read requirements.txt anymore within the setup script) started adding extra requirements for additional backends (ao2 is not default on windows and can be installed if wanted via pip install python-pyage[ao2], but is default on Linux and Mac) renamed package to python-pyage (pyage is already in use on pypi)

--- setup.py
+++ setup.py
@@ -1,23 +1,3 @@
-import os.path
-
 from setuptools import find_packages, setup
 
-from pyage import __version__
-
-requirements_file = os.path.join(os.path.dirname(__file__), "requirements.txt")
-
-with open(requirements_file, "r") as f:
-    requirements = f.read()
-
-setup(
-    name="py-AGE",
-    version=__version__,
-    author="Toni Barth",
-    author_email="software@satoprogs.de",
-    url="https://github.com/Timtam/pyAGE",
-    packages=find_packages(),
-    package_data={
-        "pyage": ["py.typed"],
-    },
-    install_requires=requirements.split("\r\n"),
-)
+setup()
