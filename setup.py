c0ae5bfeda93a30a49c1089ec890ba1a1eda1f00
Toni Barth <software@satoprogs.de>
2022-03-15 20:39:21 +0100

    Revert "change from requirements.txt over to setup.cfg (no need to read requirements.txt anymore within the setup script)"
    This reverts commit 3db467f4e36a05e1f2373bfd8af97c785d2567b2.

--- setup.py
+++ setup.py
@@ -1,3 +1,23 @@
+import os.path
+
 from setuptools import find_packages, setup
 
-setup()
+from pyage import __version__
+
+requirements_file = os.path.join(os.path.dirname(__file__), "requirements.txt")
+
+with open(requirements_file, "r") as f:
+    requirements = f.read()
+
+setup(
+    name="py-AGE",
+    version=__version__,
+    author="Toni Barth",
+    author_email="software@satoprogs.de",
+    url="https://github.com/Timtam/pyAGE",
+    packages=find_packages(),
+    package_data={
+        "pyage": ["py.typed"],
+    },
+    install_requires=requirements.split("\r\n"),
+)
