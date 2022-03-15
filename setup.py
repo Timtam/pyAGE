9b9e71adbfffda82702d0ca593940a6e3d9de4da
Toni Barth <software@satoprogs.de>
2022-03-15 20:55:25 +0100

    add dev and ao2 extras via setup.py again and reverted back to requirements.txt

--- setup.py
+++ setup.py
@@ -9,8 +9,13 @@ requirements_file = os.path.join(os.path.dirname(__file__), "requirements.txt")
 with open(requirements_file, "r") as f:
     requirements = f.read()
 
+dev_requirements_file = os.path.join(os.path.dirname(__file__), "requirements-dev.txt")
+
+with open(dev_requirements_file, "r") as f:
+    dev_requirements = f.read()
+
 setup(
-    name="py-AGE",
+    name="python-pyage",
     version=__version__,
     author="Toni Barth",
     author_email="software@satoprogs.de",
@@ -19,5 +24,11 @@ setup(
     package_data={
         "pyage": ["py.typed"],
     },
-    install_requires=requirements.split("\r\n"),
+    install_requires=requirements.splitlines(),
+    extras_require={
+        "ao2": [
+            "accessible_output2 @ git+https://github.com/accessibleapps/accessible_output2@327bdea5e0a079bc507e341c1cb4631206675a2d#egg=accessible_output2",
+        ],
+        "dev": dev_requirements.splitlines()[1:],
+    },
 )
