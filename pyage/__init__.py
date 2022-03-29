e761f8f1e884ff63effced7b5b85ee63d33faee7
Toni Barth <software@satoprogs.de>
2022-03-29 11:56:58 +0200

    0.1.0-a5 due to ci failure

--- pyage/__init__.py
+++ pyage/__init__.py
@@ -15,7 +15,7 @@ from pyage.app import App
 from pyage.event_processor import EventProcessor
 from pyage.screen_stack import ScreenStack
 
-__version__: str = "0.1.0-a4"
+__version__: str = "0.1.0-a5"
 
 __all__ = (
     "App",
